586c570a8e547c79e668f6f5257342a909cfc276
sp0t <Chenyin1g992@gmail.com>
2024-01-18 15:36:50 +0200

    [change]-some text

--- src/components/pages/Home/treatcoin.jsx
+++ src/components/pages/Home/treatcoin.jsx
@@ -7,7 +7,7 @@ export default function TreatCoin() {
             <div className="text-white md:w-[560px] text-left flex flex-col gap-[18px]">
                 <div className="text-[36px] font-semibold leading-10">What is TreatCoin?</div>
                 <div className="text-[14px] font-normal">
-                    TreatCoin is an environment where our users will be touched by your ads. We're bringing up new ways to teach your desired customer about your product, before he's hot enough for your call/approach.
+                    TreatCoin is a vibrant space where your ads leave a lasting impression. We introduce innovative methods to educate your prospective customers about your product, capturing their interest even before they're primed for your call or approach.
                 </div>
                 <div className="text-[14px] font-normal">
                     We ensure our users are not just happy but deeply engaged, allowing you to extract optimal value from our system. It's a straightforward win-win situation!
